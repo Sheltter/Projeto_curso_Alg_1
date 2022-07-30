e249a988ed014251f8a390223fbef684db1f5675
Sheltter <72775675+Sheltter@users.noreply.github.com>
2022-07-30 16:47:44 -0300

    Update Operacoes logicas.js

--- Operacoes logicas.js
+++ Operacoes logicas.js
@@ -6,7 +6,7 @@ nome = prompt("Digite o nome do aluno:")
 nota01 = prompt("Informe a nota 01")
 nota02 = prompt("Informe nota 02")
 
-media = (parseint(Nota01)+ parseint(nota02)) /2;
+media = (parseint(nota01)+ parseint(nota02)) /2;
 
 if (media >= 50)
     passou = true;
